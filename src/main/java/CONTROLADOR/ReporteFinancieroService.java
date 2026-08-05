73f5e3fa9952ecc5fc65f911f3bf0a5bfe041b6d
Anyelo <Anyelo@DESKTOP-B1F9G3T>
2026-08-05 09:00:17 -0500

    Mejorada logica de reportes

--- src/main/java/CONTROLADOR/ReporteFinancieroService.java
+++ src/main/java/CONTROLADOR/ReporteFinancieroService.java
@@ -14,36 +14,31 @@ import java.util.stream.Collectors;
 
 public class ReporteFinancieroService {
     public void ReportedeIngresosMensualesporMarca(){
+        VentasDAO vdao = new VentasDAO();
+        List<Ventas> lista = vdao.listar();
         
         Detalle_VentaDAO dvdao = new Detalle_VentaDAO();
-        List<Detalle_Venta> lista = dvdao.listar();
+        List<Detalle_Venta> listamarca = dvdao.listar();
 
         if (lista == null || lista.isEmpty()) {
             System.out.println("No hay datos de ventas.");
-                    System.out.println(lista);
+                    System.out.println(listamarca);
             return;
         }
-    
-        Map<Double, Integer> top = lista.stream().filter(dv -> dv.getCelular().getMarca() != null).collect(Collectors.groupingBy(dv -> dv.getPrecio_unitario(),Collectors.summingInt(Detalle_Venta::getCantidad)));
-
-       
+            
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("reporte_ingresos_marcas.txt"))) {
 
             writer.write("=== Reporte de ingresos por marca ===\n");
             
-            topm.entrySet().stream().sorted((a, b) -> b.getValue().compareTo(a.getValue())).limit(3).forEach(e -> {
-                
-                String marca = (e.getKey() != null) ? e.getKey() : "Desconocido";String linea = "Marca: " + marca + " | Vendidos: " + e.getValue();
-                
-                System.out.println(linea);
-                
-                try {
-                    writer.write(linea);
-                    writer.newLine();
-                } catch (IOException ex) {
-                    System.out.println(ex.getMessage());
+            listamarca.stream().filter(dv -> dv.getVenta() != null).forEach(dv -> {String marcas = "Marca" + listamarca + " total: " + dv.getCantidad();
+       
+            try {
+                writer.write(marcas + "\n");
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
                 }
             });
+            
 
             System.out.println("Reporte generado reporte_ingresos_marcas.txt");
 
